e126f20d2e86cf9476ef5dad49c85816a8e464f7
johashei <j.s.heines@fys.uio.no>
2022-02-14 13:27:29 +0100

    initial commit

---
+++ simfit/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.4.0.0"
+__author__ = "Johannes Sørby Heines"
+
+"""
+Version legend:
+a.b.c.d
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
+from .fitting import *
+from peakfit import *
